0bded2edfde1391b78dde688bb00fd6fb60f5449
MrBanana12344 <61161613+MrBanana12344@users.noreply.github.com>
2024-11-07 18:43:45 -0500

    Attepmpted to make the buttons bigger, to no success

--- obj/Debug/net7.0/TicTacToeWeb.AssemblyInfo.cs
+++ obj/Debug/net7.0/TicTacToeWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b78cdf82cc79e8a67dcad18f0e910b38761d195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e0465d47f33ee7a4ed997fca700a30a882fccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
